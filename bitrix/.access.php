03bcd47f329ae4c92da57e173e8bcd7f5ca1d092
Ivanov Andrey <sttten@gmail.com>
2015-10-13 20:59:11 +0300

    git-repository creation

---
+++ bitrix/.access.php
@@ -0,0 +1,8 @@
+<?
+$PERM["admin"]["12"]="T_8";
+$PERM["admin"]["9"]="T_8";
+$PERM["admin"]["8"]="T_8";
+$PERM["admin"]["6"]="T_8";
+$PERM["admin"]["5"]="T_8";
+$PERM["admin"]["*"]="D";
+?>
---
+++ bitrix/modules/main/admin/define.php
@@ -0,0 +1,1 @@
+<?define("TEMPORARY_CACHE", "ARttewYHbmMMdAgebRtkG24A");?>
